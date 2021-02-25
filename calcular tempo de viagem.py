ab8b421adb4a5aa8743345b767218286338eb5d6
Nicolas Oliveira <79637247+YuriNick-jpg@users.noreply.github.com>
2021-02-25 16:15:32 -0300

    Add files via upload

---
+++ calcular tempo de viagem.py
@@ -0,0 +1,8 @@
+distância = int(input('que distância será percorrida em km? '))
+print()
+velocidade = int(input('A que velocidade em km/h? '))
+tempo = distância/velocidade
+print()
+print('seu tempo de viagem em horas será de:')
+print()
+print("%.2f" % tempo)
