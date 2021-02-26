54f8b59416aa11b32509ec280b55f9229ff3d264
Nicolas Oliveira <79637247+YuriNick-jpg@users.noreply.github.com>
2021-02-26 08:14:46 -0300

    Add files via upload

---
+++ conversor de F para C.py
@@ -0,0 +1,8 @@
+print('Vamos converter uma temperatura de Fahrenheit para Celsius.')
+print()
+f = int(input('Temperatura em Fahrenheit:'))
+print()
+c = 5 * (f - 32) / 9
+print('Em Celsius:')
+print()
+print(c)
